b750f4c26a5cc1793a4d9de993b7fac27acfde16
Raimo Tuisku <raimo.tuisku@gmail.com>
2013-02-15 15:40:52 -0800

    Raidmo - an Identity Provider

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raidmo::Application.config.secret_token = '1bbb19b93112b320c3702ab622d4b12f91dfa65584f86d6893d23d5b6983a75de783ed5c17c2e95ffe89f3483f039da96b6ad9cedaf7be98de13b289bf353b21'
